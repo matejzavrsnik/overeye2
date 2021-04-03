5b4ec56f672a5c9034291e2fa798519392fdabb0
matej <matejzavrsnik@gmail.com>
2021-04-03 21:06:11 +0100

    refactor: factory of gauges had annoying code duplication

--- library/src/gauge/factory.cpp
+++ library/src/gauge/factory.cpp
@@ -11,77 +11,70 @@
 namespace gauge
 {
 
+std::unique_ptr<gauge::webport>
+instantiate_webport(
+   gauge::type type,
+   std::wstring stylesheet,
+   std::shared_ptr<gauge_settings> settings
+)
+{
+   switch(type)
+   {
+   case gauge::type::webport:
+      return std::make_unique<gauge::webport>(stylesheet, settings);
+   case gauge::type::clock:
+      return std::make_unique<gauge::clock>(stylesheet, settings);
+   case gauge::type::twitter:
+      return std::make_unique<gauge::twitter>(stylesheet, settings);
+   }
+
+   throw std::exception{}; // todo: make my own
+}
+
+std::unique_ptr<representation>
+webport_gauge_factory(
+   gauge::type type,
+   const std::vector<gauge::basic_setting>& settings,
+   const configuration& gc,
+   const std::wstring& stylesheet
+)
+{
+   auto gauge_representation = std::make_unique<representation>();
+
+   // Instantiate and apply custom settings
+   auto logical = instantiate_webport(type, stylesheet, gauge_representation->parameters);
+   logical->apply_user_settings(settings);
+   auto visual = std::make_unique<gui::webport>(gauge_representation->parameters);
+   visual->setObjectName(std::string("webport") + std::to_string(gauge_representation->unique.id()));
+
+   // Connect signals and slots
+   visual->request_content.connect(&gauge::webport::display, logical.get());
+   visual->signal_settings_changed.connect(&gauge::webport::display, logical.get());
+   logical->send_content.connect(&gui::webport::receive_content, visual.get());
+   logical->request_refresh.connect(&gui::webport::receive_request_refresh, visual.get());
+
+   // Assemble into representation object
+   gauge_representation->logical = std::move(logical);
+   gauge_representation->visual = std::move(visual);
+   gauge_representation->location = gc.location;
+
+   return gauge_representation;
+}
 
 std::unique_ptr<representation>
 gauge_factory (
    const configuration& gc,
-   const library::settings& set
+   const library::settings& settings
 )
 {
-   mzlib::unique unique;
-   auto r = std::make_unique<representation>();
-
-   switch (gc.type)
+   switch(gc.type)
    {
    case gauge::type::webport:
-   {
-      auto logical = std::make_unique<gauge::webport>(set.gauge_stylesheet, r->parameters);
-      logical->apply_user_settings(gc.settings);
-      auto visual = std::make_unique<gui::webport>(r->parameters);
-      visual->setObjectName(std::string("webport") + std::to_string(unique.id()));
-
-
-      visual->request_content.connect(&gauge::webport::display, logical.get());
-      visual->signal_settings_changed.connect(&gauge::webport::display, logical.get());
-      logical->send_content.connect(&gui::webport::receive_content, visual.get());
-      logical->request_refresh.connect(&gui::webport::receive_request_refresh, visual.get());
-
-      r->unique = std::move(unique);
-      r->logical = std::move(logical);
-      r->visual = std::move(visual);
-      r->location = gc.location;
-
-      return r;
-   }
-   case gauge::type::twitter:
-   {
-      auto logical = std::make_unique<gauge::twitter>(set.gauge_stylesheet, r->parameters);
-      logical->apply_user_settings(gc.settings);
-      auto visual = std::make_unique<gui::webport>(r->parameters);
-      visual->setObjectName(std::string("twitter") + std::to_string(unique.id()));
-
-      visual->request_content.connect(&gauge::webport::display, logical.get());
-      visual->signal_settings_changed.connect(&gauge::webport::display, logical.get());
-      logical->send_content.connect(&gui::webport::receive_content, visual.get());
-      logical->request_refresh.connect(&gui::webport::receive_request_refresh, visual.get());
-
-      r->unique = std::move(unique);
-      r->logical = std::move(logical);
-      r->visual = std::move(visual);
-      r->location = gc.location;
-
-      return r;
-   }
    case gauge::type::clock:
-   {
-      auto logical = std::make_unique<gauge::clock>(set.gauge_stylesheet, r->parameters);
-      logical->apply_user_settings(gc.settings);
-      auto visual = std::make_unique<gui::webport>(r->parameters);
-      visual->setObjectName(std::string("clock") + std::to_string(unique.id()));
-
-      visual->request_content.connect(&gauge::webport::display, logical.get());
-      visual->signal_settings_changed.connect(&gauge::webport::display, logical.get());
-      logical->send_content.connect(&gui::webport::receive_content, visual.get());
-      logical->request_refresh.connect(&gui::webport::receive_request_refresh, visual.get());
-
-      r->unique = std::move(unique);
-      r->logical = std::move(logical);
-      r->visual = std::move(visual);
-      r->location = gc.location;
-
-      return r;
+   case gauge::type::twitter:
+      return webport_gauge_factory(gc.type, gc.settings, gc, settings.gauge_stylesheet);
    }
-   }
+
    throw std::exception{}; // todo: make my own
 }
 
